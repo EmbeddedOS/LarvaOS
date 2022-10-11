f651104336ec37ea6a273c99c6f23bf184591f2f
Larva <nguyenthanhcong12gtm1@gmail.com>
2022-10-12 00:36:39 +0700

    Add detail comments.

--- src/kernel/arch/x86/boot/boot.asm
+++ src/kernel/arch/x86/boot/boot.asm
@@ -1,18 +1,49 @@
-ORG 0x7C00  ; Assembly origin. 
-BITS 16     ; 16 bit instruction.         
+; -- This is a Boot sector. BIOS will look for this as the first thing!
+
+
+ORG 0x7C00  ; Why do we need to select the MBR(Master Boot Record) at 0x7C00(32KB - 1KB) address of the memory?
+            ; Minimum requirement for DOS - Disk Operating System version 1.0 was 32KB (0x0000 to 0x7FFF). 
+            ; When we subtract 1KB (1024 bytes) from 32KB we will ge the value 0x7C00.
+            ; Even though the length of MBR is 512 bytes,
+            ; the reason for keeping 1024 bytes for MBR is to have extra memory for Stack/Data area of MBR.
+            ; For more details: https://www.glamenv-septzen.net/en/view/6
+            ;
+            ; Early version of x86 has the memory architecture was like below:
+            ; 0x0000    +---------------------------------------+
+            ;           | Interrupts vector table (1024 bytes). |
+            ; 0x0400    +---------------------------------------+
+            ;           | BIOS Data area (256 bytes).           |
+            ; 0x0500    +---------------------------------------+
+            ;           | OS load area (30,464 bytes).          |
+            ; 0x7C00    +---------------------------------------+
+            ;           | MBR (512 bytes).                      |
+            ; 0x7E00    +---------------------------------------+
+            ;           | Boot Stack/Data area (512 bytes).     |
+            ; 0x7FFF    +---------------------------------------+
+
+
+            ; The ORG directive assigns a specific offset to the very next label or instruction following the
+            ; directive. The addresses of all subsequent labels are calculated from that point onward.
+            ; In our case, the address 0x7C00 will be asigned to 'start' label.
+
+BITS 16     ; 16 bit instruction.      
+            ; Since all intel based start up with 16-bit instructions,
+            ; we have to start with 16-bit instructions before switch to protected mode to use 32-bit instructions.
 
 CODE_SEG equ gdt_code - gdt_start
 DATA_SEG equ gdt_data - gdt_start
 
 ; -- Entry point.
-jmp short _start 
+jmp short _start
 nop
 
-
+; -- OEM Parameter Block / BIOS Parameter Block.
 times 33 db 0   ; Set up BIOS parameter block, this is necessary to avoid corrupt data. 
 
 _start:
-    jmp 0:start
+    jmp 0:start ; by jumping to segment 0 and address offset 0x7C00, 
+                ; we ensure that absolute address calculation will always get the correct address.
+
 
 start:
     ; -- Set up memory:
@@ -31,16 +62,20 @@ start:
     ; 2. Enable A20 Line.
     ; 3. Load the Global Descriptor Table with segment descriptors suitable for code, data and stack.
     cli                     ; Disable interrupts.
-    lgdt[gdt_descriptor]    ; Load Global Descriptor Table register with start address of GDT.
+    lgdt[gdt_descriptor]    ; Load Global Descriptor Table register with start address of GDT,
+                            ; which defines the protected mode segments (e.g for code and data).
     
-    mov eax, cr0
-    or al, 0x1              ; Set Protection Enable - PE bit in CR0 (Control register 0).
+    mov eax, cr0            ; To make the switch to protected mode, we set
+    or al, 0x1              ; the first bit Protection Enable - PE bit in CR0 (Control register 0).
     mov cr0, eax 
-    jmp CODE_SEG:protect_mode_main  ; Perform far jump to selector CODE_SEG (offset into GDT, pointing at a 32 bit PM code segment descriptor)
-                                    ; to load CS with proper PM32.
+    jmp CODE_SEG:start_protected_mode   ; Perform far jump to selector CODE_SEG (offset into GDT, pointing at a 32 bit PM code segment descriptor)
+                                        ; to load CS with proper PM32.
+                                        ; Using far jump also forces the CPU to flush its cache of pre-fetched and
+                                        ; real-mode decoded instructions, which can cause problems.
 
 
 
+; Global Descriptor Table memory layout.
 ; Global descriptor table setup.
 gdt_start:
 gdt_null:
@@ -72,8 +107,8 @@ gdt_descriptor:
     dd gdt_start
 
 [BITS 32]
-protect_mode_main:
-    ; -- Entered Protected Mode.
+start_protected_mode:
+    ; By now we are assuredly in 32 - bit protected mode.
     ; load DS, ES, FS, GS, SS, ESP
     mov ax, DATA_SEG
     mov ds, ax 
@@ -84,6 +119,12 @@ protect_mode_main:
     mov ebp, 0x00200000
     jmp $
 
-times 510-($ - $$) db 0
-dw 0xAA55
+times 510-($ - $$) db 0             ; When compiled, our program must fit into 512 bytes,
+                                    ; with the last two bytes being the magic number,
+                                    ; so here, tell our assembly compiler t pad out our
+                                    ; program with enough zeros bytes (db 0) to bring us 
+                                    ; to the 510th byte.
+
+dw 0xAA55                           ; Last two byte (one word) form the magic number,
+                                    ; So BIOS knows we are a boot sector (as bootable).
 
