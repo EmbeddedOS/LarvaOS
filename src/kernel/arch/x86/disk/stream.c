b4dc5d025721c371832cd37d6e2d8b03ae68dbd2
Larva <nguyenthanhcong12gtm1@gmail.com>
2023-01-11 08:37:16 +0700

    Implement disk stream part 2.

--- src/kernel/arch/x86/disk/stream.c
+++ src/kernel/arch/x86/disk/stream.c
@@ -1,4 +1,5 @@
 #include <mm/kheap.h>
+#include <stdbool.h>
 #include "stream.h"
 
 struct disk_stream *create_disk_stream(int disk_id)
@@ -23,24 +24,35 @@ int disk_stream_seek(struct disk_stream *stream, int pos)
 
 int disk_stream_read(struct disk_stream *stream, void *buf, int total)
 {
+    bool need_to_read_more_than_one_sector = (total > DISK_SECTOR_SIZE);
     int sector = stream->pos / DISK_SECTOR_SIZE;
     int offset = stream->pos % DISK_SECTOR_SIZE;
     char tmp_buf[DISK_SECTOR_SIZE];
     int result = 0;
+
+    // Read a block.
     result = disk_read_blocks(stream->disk, sector, 1, tmp_buf);
     if (result < 0)
     {
         goto out;
     }
 
-    int total_to_read = (total > DISK_SECTOR_SIZE) ? DISK_SECTOR_SIZE : total;
+    int total_to_read = need_to_read_more_than_one_sector ? DISK_SECTOR_SIZE : total;
 
     for (int i = 0; i < total_to_read; i++)
     {
         *(char *)buf++ = tmp_buf[offset + i];
     }
 
-    // TODO
+    // Adjust the stream position.
+    stream->pos += total_to_read;
+
+    // Recursive until read full `total` bytes.
+    if (need_to_read_more_than_one_sector)
+    {
+        result = disk_stream_read(stream, buf, (total - total_to_read));
+    }
+
 out:
     return result;
 }
