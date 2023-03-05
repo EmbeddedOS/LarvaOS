3becb9d37814572a189fbbbae161e970cf542c04
Larva <nguyenthanhcong12gtm1@gmail.com>
2023-03-05 12:15:38 +0700

    Implement lava::move() function.

---
+++ src/kernel/include/utility.hh
@@ -0,0 +1,26 @@
+#pragma once
+
+namespace lava
+{
+    template <class T>
+    struct remove_reference
+    {
+        typedef T type;
+    };
+    template <class T>
+    struct remove_reference<T &>
+    {
+        typedef T type;
+    };
+    template <class T>
+    struct remove_reference<T &&>
+    {
+        typedef T type;
+    };
+
+    template <typename T>
+    typename remove_reference<T>::type &&move(T &&arg)
+    {
+        return static_cast<typename remove_reference<T>::type &&>(arg);
+    }
+}
